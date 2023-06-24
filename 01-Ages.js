d9ae30776dad559da5fee943a4145aaa0d73bb3a
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2023-06-24 15:57:25 +0300

    Add files via upload

---
+++ 01-Ages.js
@@ -0,0 +1,24 @@
+function solve(age){
+    if(age < 0){
+        console.log('out of bounds');
+        return;
+    }
+
+    if(age >= 66){
+        console.log('elder');
+    }
+    else if(age >= 20){
+        console.log('adult');
+    }
+    else if(age >= 14){
+        console.log('teenager');
+    }
+    else if(age >= 3){
+        console.log('child');
+    }
+    else if(age >= 0){
+        console.log('baby');
+    }
+}
+
+solve(-1);
