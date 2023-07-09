a7bbc7fae8715fd9651e0a8887affb84a98829e6
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2023-07-09 16:27:36 +0300

    Add files via upload

---
+++ 02-Towns.js
@@ -0,0 +1,13 @@
+function solve(input){
+    input.map((city) => {
+        const [town, latitude, longitude] = city.split(" | ");
+        return {town, latitude, longitude};
+    })
+    .forEach((city) => {
+        console.log(`{ town: '${city.town}', latitude: '${Number(city.latitude).toFixed(2)}', longitude: '${Number(city.longitude).toFixed(2)}' }`);  
+    });
+
+    
+}
+
+solve(['Sofia | 42.696552 | 23.32601', 'Beijing | 39.913818 | 116.363625']);
