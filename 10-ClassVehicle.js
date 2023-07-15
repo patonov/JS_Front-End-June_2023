86e990039b0cee2bcc28d7b55ca45cdf49425903
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2023-07-15 16:41:42 +0300

    Add files via upload

---
+++ 10-ClassVehicle.js
@@ -0,0 +1,18 @@
+class Vehicle{
+    constructor(type, model, parts, fuel){
+        this.type = type;
+        this.model = model;
+        this.parts = parts;
+        this.parts.quality = parts.engine * parts.power;
+        this.fuel = Number(fuel);
+    }
+    drive(loss){
+        return this.fuel -= Number(loss);
+    }
+}
+
+
+let parts = {engine: 9, power: 500};
+let vehicle = new Vehicle('l', 'k', parts, 840);
+vehicle.drive(20);
+console.log(vehicle.fuel);
