b534c970bbfd4025c6712652f1da90bd413f3101
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2023-07-22 17:00:16 +0300

    Add files via upload

---
+++ 05. Fill-Dropdown/dropdown.js
@@ -0,0 +1,13 @@
+function addItem() {
+    const textInput = document.querySelector("#newItemText");
+    const valueInput = document.querySelector("#newItemValue");
+
+    const option = document.createElement("option");
+    option.textContent = textInput.value;
+    option.setAttribute("value", valueInput.value);
+
+    const select = document.querySelector("#menu");
+    select.appendChild(option);
+    textInput.value = "";
+    valueInput.value = "";
+}
