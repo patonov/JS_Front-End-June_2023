2212de8e9f7cad5fdf8de061aadca3105e46049e
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2023-07-27 17:57:34 +0300

    Add files via upload

---
+++ 02.Bus-Schedule/app.js
@@ -0,0 +1,42 @@
+function solve() {
+    const departButton = document.querySelector("#depart");
+    const arriveButton = document.querySelector("#arrive");
+    const bussInfoBox = document.querySelector("#info .info");
+
+    let busStopInfo = {
+        name: "",
+        next: "depot",
+    };
+
+
+    function depart() {
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${busStopInfo.next}`)
+        .then((res) => res.json())
+        .then((busStop) => {
+            busStopInfo = busStop;
+            departButton.disabled = true;
+            arriveButton.disabled = false;
+            bussInfoBox.textContent = `Next stop ${busStopInfo.name}`;
+        })
+        .catch(() => {
+            departButton.disabled = false;
+            arriveButton.disabled = false;
+            bussInfoBox.textContent = "Error";
+        })
+
+
+    }
+
+    async function arrive() {
+        departButton.disabled = false;
+        arriveButton.disabled = true;
+        bussInfoBox.textContent = `Arriving at ${busStopInfo.name}`;
+    }
+
+    return {
+        depart,
+        arrive
+    };
+}
+
+let result = solve();
